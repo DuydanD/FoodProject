fbb2f14e3c29ecae1ac271cf6a2a3b219f5c8c6b
Duydan <dandinh99@gmail.com>
2025-05-26 21:32:09 +0100

    Add close button to pop up window.

--- myproject/.venv/static/script.js
+++ myproject/.venv/static/script.js
@@ -1,6 +1,11 @@
 const addIngredientBtn = document.getElementById('addIngredientBtn');
 const ingredientPopup = document.getElementById('ingredientPopup');
+const closePopupBtn = document.getElementById('closePopupBtn');
 
 addIngredientBtn.addEventListener('click', () => {
     ingredientPopup.style.display = 'block';
  });
+
+closePopupBtn.addEventListener('click', () =>{
+    ingredientPopup.style.display = 'none';
+});
