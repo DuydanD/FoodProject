5352e267114e9b97cbc04e16a6c0d281763b528f
Duydan <dandinh99@gmail.com>
2025-05-29 19:08:44 +0100

    Removed delete selected button till an ingredient is selected

--- myproject/.venv/static/script.js
+++ myproject/.venv/static/script.js
@@ -1,6 +1,8 @@
 const addIngredientBtn = document.getElementById('addIngredientBtn');
 const ingredientPopup = document.getElementById('ingredientPopup');
 const closePopupBtn = document.getElementById('closePopupBtn');
+const deletedSelectedBtn = document.getElementById('deleteSelectedBtn')
+const deleteIngredient = document.querySelectorAll('input[name="delete_ingredient"]')
 
 addIngredientBtn.addEventListener('click', () => {
     ingredientPopup.style.display = 'block';
@@ -9,3 +11,26 @@ addIngredientBtn.addEventListener('click', () => {
 closePopupBtn.addEventListener('click', () =>{
     ingredientPopup.style.display = 'none';
 });
+
+
+function toggleDeleteButtonState() {
+    let checked = false;
+
+    for (const checkbox of deleteIngredient) {
+        if (checkbox.checked) {
+            checked = true;
+            break;
+        }
+    }
+    if (checked) {
+        deletedSelectedBtn.style.display = 'inline-block';
+    } else {
+        deletedSelectedBtn.style.display = 'none';
+    }
+}
+
+deleteIngredient.forEach(checkbox => {
+    checkbox.addEventListener('change', toggleDeleteButtonState);
+})
+
+toggleDeleteButtonState();
